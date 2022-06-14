87d9a0620f32f8a351932495e2ea4c809c68d8d7
KUNKUN2121 <kunkun7532@gmail.com>
2022-06-14 12:40:43 +0000

    退出処理 カード配布処理

--- server.js
+++ server.js
@@ -47,20 +47,21 @@ const card = [
   "MAX->0",
   "?",
 ];
-rand = Math.floor(Math.random()*card.length);
+// rand = Math.floor(Math.random()*card.length);
 
-console.log('rand is ' + rand+'');
-console.log('cardrand is ' + card[rand]);
-console.log('array is ' + card);
-card.splice(rand, 1);
-console.log('arrayis2'+card);
-
+// console.log('rand is ' + rand+'');
+// console.log('cardrand is ' + card[rand]);
+// console.log('array is ' + card);
+// card.splice(rand, 1);
+// console.log('arrayis2'+card);
+var shuffledCard = arrayShuffle(card);
 let i
 var userAry = [];
   
 
 io.sockets.on('connection', function(socket) {
     var name;
+    var game_started;
 
 
     // S05. client_to_serverイベント・データを受信する
@@ -70,6 +71,19 @@ io.sockets.on('connection', function(socket) {
         io.sockets.emit('server_to_client', {value : data.value});
     });
 
+    // スタートPost
+    socket.on('start_post', function(data) {
+      // S06. server_to_clientイベント・データを送信する
+      console.log('start 処理です  : '+data.value);
+      io.sockets.emit('start_post', {value : data.value});
+
+      //ユーザーID分 繰り返し 
+      for(i=0; i< userAry.length; i++){
+        var selectcard = shuffledCard.pop(); 
+        io.to(userAry[i]).emit('card_post', {value : selectcard});
+        console.log(userAry[i]+' '+selectcard);
+      };
+  });
 
 
     // S07. client_to_server_broadcastイベント・データを受信し、送信元以外に送信する
@@ -82,6 +96,8 @@ io.sockets.on('connection', function(socket) {
     // 個人送信 
     socket.on('client_to_server_personal', function(data) {
         var id = socket.id;
+        userAry.push(id);
+        console.log(userAry);
         name = data.value;
         var personalMessage = "あなたは、" + name + "さんとして入室しました。"
         io.to(id).emit('server_to_client', {value : personalMessage});
@@ -94,8 +110,35 @@ io.sockets.on('connection', function(socket) {
         if (name == 'undefined') {
             console.log("未入室のまま、どこかへ去っていきました。");
         } else {
+            var val = socket.id
+            var idx = userAry.indexOf(val);
+            if(idx >= 0){
+            userAry.splice(idx, 1); 
+            }
+            console.log('userAry : ', userAry);
             var endMessage = name + "さんが退出しました。"
             io.sockets.emit('server_to_client', {value : endMessage});
         }
     });
 });
+
+
+
+
+
+// 配列シャッフル 
+// 参考 : https://gray-code.com/javascript/shuffle-for-item-of-array/
+
+function arrayShuffle(array) {
+  for(var i = (array.length - 1); 0 < i; i--){
+
+    // 0〜(i+1)の範囲で値を取得
+    var r = Math.floor(Math.random() * (i + 1));
+
+    // 要素の並び替えを実行
+    var tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+  return array;
+}
